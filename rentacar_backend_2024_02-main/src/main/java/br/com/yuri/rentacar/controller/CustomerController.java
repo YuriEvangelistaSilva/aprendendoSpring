81b17500c73e72f58c7dfe7bdcd350d689821417
Yuri Evangelista Silva <yurievangelistasilva@gmail.com>
2024-10-16 11:42:08 -0300

    acrecentado documentacao do customerController

--- rentacar_backend_2024_02-main/src/main/java/br/com/yuri/rentacar/controller/CustomerController.java
+++ rentacar_backend_2024_02-main/src/main/java/br/com/yuri/rentacar/controller/CustomerController.java
@@ -2,13 +2,20 @@ package br.com.yuri.rentacar.controller;
 
 import br.com.yuri.rentacar.dto.CustomerDto;
 import br.com.yuri.rentacar.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag( name = "Customers", description = "Endpint para operaçoes emvolvendo Costumers")
 @RestController
 @RequestMapping("/Customer")
 public class CustomerController {
@@ -16,6 +23,13 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Cria ou cadastra um novo Customer (cliente)", tags = {"Customer"} responces = {
+            @ApiResponse(description = "CREATED", responseCode = "201", content = {@Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = CustomerDto.class)
+            )})
+    })
     public ResponseEntity<CustomerDto> create(@RequestBody CustomerDto customerDto){
         CustomerDto customer = customerService.create(customerDto);
         return new ResponseEntity<>(customer, HttpStatus.CREATED);
